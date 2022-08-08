135522440bdbcb5a30fe3fe6227530d70b601114
fatiudeen <shehufatiudeen@gmail.com>
2022-08-08 14:00:49 +0100

    identidication

---
+++ src/routes/id.route.ts
@@ -0,0 +1,25 @@
+import idController from '@controllers/id.controller';
+import { verifyidentityNumberDto } from '@dtos/users.dto';
+import { Routes } from '@interfaces/routes.interface';
+import validationMiddleware from '@middlewares/validation.middleware';
+import { Router } from 'express';
+
+class VerifyRoute implements Routes {
+  public path = '/id/';
+  public router = Router();
+  public idController = new idController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(`${this.path}nin`, validationMiddleware(verifyidentityNumberDto, 'body'), this.idController.getNIN);
+    this.router.post(`${this.path}vin`, validationMiddleware(verifyidentityNumberDto, 'body'), this.idController.getVotersCard);
+    this.router.post(`${this.path}passport`, validationMiddleware(verifyidentityNumberDto, 'body'), this.idController.getInternationalPassport);
+    this.router.post(`${this.path}drivers-license`, validationMiddleware(verifyidentityNumberDto, 'body'), this.idController.getDriversLicense);
+    this.router.post(`${this.path}bvn`, validationMiddleware(verifyidentityNumberDto, 'body'), this.idController.getBVN);
+  }
+}
+
+export default VerifyRoute;
